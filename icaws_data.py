84774e7d9105c7ee3b6dd1abed474294a3c1a841
Henry Hunt <henryhunt.dev@gmail.com>
2018-03-25 20:10:48 +0100

    Added configuration global and loader

--- icaws_data.py
+++ icaws_data.py
@@ -2,3 +2,14 @@
 #  responsible for measuring and logging data parameters, and for generating
 #  statistics. All database writes are done here. This is the main ICAWS
 #  program and the entry point for the ICAWS software
+
+import config_data
+
+config = config_data.ConfigData()
+
+
+if __name__ == "__main__":
+    config_load = config.load()
+
+    # Cannot start ICAWS software without a configuration
+    if config_load == False: system.exit(1)
