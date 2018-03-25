222fbf8d4e8ea4eb117751b01368da263381d88b
Henry Hunt <henryhunt.dev@gmail.com>
2018-03-25 20:14:09 +0100

    Added ConfigData class with load() function

---
+++ config_data.py
@@ -0,0 +1,16 @@
+import os
+import configparser
+
+class ConfigData():
+    database_path = None
+
+    # Loads the config.ini file from the current directory
+    def load():
+        if not os.path.isfile("config.ini"):
+            return False
+
+        parser = configparser.ConfigParser()
+        config.read("config.ini")
+
+        database_path = config.get("DataStores", "DatabasePath")
+        return True
