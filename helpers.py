3f6c965df87ba46750c16b736714df3b06065172
Henry Hunt <henryhunt.dev@gmail.com>
2018-03-26 15:38:05 +0100

    Added example code to get actual remaining space

--- helpers.py
+++ helpers.py
@@ -1,5 +1,12 @@
 import os
 
 def remaining_space(directory):
-    analysis = os.statvfs(directory)
-    return analysis.f_frsize * analysis.f_bavail
+    disk = os.statvfs(directory)
+
+    totalBytes = float(disk.f_bsize*disk.f_blocks)
+    totalUsedSpace = float(disk.f_bsize*(disk.f_blocks-disk.f_bfree))
+    totalAvailSpace = float(disk.f_bsize*disk.f_bfree)
+    totalAvailSpaceNonRoot = float(disk.f_bsize*disk.f_bavail)
+
+    print("available space for non-super user: %d Bytes = %.2f KBytes = %.2f MBytes = %.2f GBytes " % (totalAvailSpaceNonRoot, totalAvailSpaceNonRoot/1024, totalAvailSpaceNonRoot/1024/1024, totalAvailSpaceNonRoot/1024/1024/1024))
+    return disk.f_frsize * disk.f_bavail
