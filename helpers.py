7bc41a606b911b7921df86ce9e4322867b1d6901
Henry Hunt <henryhunt.dev@gmail.com>
2018-03-26 15:43:53 +0100

    Fixed remaining_space() function

--- helpers.py
+++ helpers.py
@@ -1,12 +1,9 @@
 import os
 
 def remaining_space(directory):
+    """ Returns the amount of remaining space for non-root users in gigabytes
+    """
     disk = os.statvfs(directory)
 
-    totalBytes = float(disk.f_bsize*disk.f_blocks)
-    totalUsedSpace = float(disk.f_bsize*(disk.f_blocks-disk.f_bfree))
-    totalAvailSpace = float(disk.f_bsize*disk.f_bfree)
-    totalAvailSpaceNonRoot = float(disk.f_bsize*disk.f_bavail)
-
-    print("available space for non-super user: %d Bytes = %.2f KBytes = %.2f MBytes = %.2f GBytes " % (totalAvailSpaceNonRoot, totalAvailSpaceNonRoot/1024, totalAvailSpaceNonRoot/1024/1024, totalAvailSpaceNonRoot/1024/1024/1024))
-    return disk.f_frsize * disk.f_bavail
+    non_root_space = float(disk.f_bsize * disk.f_bavail)
+    return totalAvailSpaceNonRoot / 1024 / 1024 / 1024
