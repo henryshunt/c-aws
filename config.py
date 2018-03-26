d1d389aed28ba08ff3166d016b5e92efdbd66adf
Henry Hunt <henryhunt.dev@gmail.com>
2018-03-26 16:23:19 +0100

    Added more config.ini parameters to ConfigData

--- config.py
+++ config.py
@@ -7,6 +7,8 @@ class ConfigData():
         """ Initialises instance variables for each config.ini parameter
         """
         self.database_path = None
+        self.camera_drive = None
+        self.backup_drive = None
 
     def load(self):
         """ Loads the data from the config.ini file in the current directory
@@ -18,4 +20,6 @@ class ConfigData():
             parser.read("config.ini")
 
             self.database_path = parser.get("DataStores", "DatabasePath")
+            self.camera_drive = parser.get("DataStores", "CameraDrive")
+            self.backup_drive = parser.get("DataStores", "BackupDrive")
         except: return None
