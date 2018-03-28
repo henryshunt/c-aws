23240d750753cb3a572479429a75f3564b499434
Henry Hunt <henryhunt.dev@gmail.com>
2018-03-28 20:57:21 +0100

    Removed obsolete utc_to_local() function

--- helpers.py
+++ helpers.py
@@ -26,9 +26,8 @@ def exit(code):
                                .strftime("%Y-%m-%d %H:%M:%S"), code))
 
             gpio.output(23, gpio.HIGH)
-            sys.exit(1)
-            
-    except: sys.exit(1)
+    except: pass
+    sys.exit(1)
 
 def exit_no_light(code):
     try:
@@ -36,18 +35,12 @@ def exit_no_light(code):
             file.write("initialisation failure at {} with exit code {}"
                        .format(datetime.now()
                                .strftime("%Y-%m-%d %H:%M:%S"), code))
-            sys.exit(1)
-
-    except: sys.exit(1)
+    except: pass
+    sys.exit(1)
 
 def init_success():
     try:
         with open("init.txt", "w+") as file:
             file.write("initialisation success at {}"
                        .format(datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
-
     except: sys.exit(1)
-
-def utc_to_local(utc, timezone):
-    utc_time = pytz.utc.localize(utc)
-    return (utc_time.astimezone(pytz.timezone(timezone)).replace(tzinfo = None))
