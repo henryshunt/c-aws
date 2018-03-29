d972f3ca6c1564ab5816cf7644d3baf3ec7c1d21
Henry Hunt <henryhunt.dev@gmail.com>
2018-03-29 14:02:39 +0100

    Moved instance variable init out of constructor

--- config.py
+++ config.py
@@ -3,43 +3,39 @@ from configparser import ConfigParser
 import pytz
 
 class ConfigData():
-    
-    def __init__(self):
-        """ Initialises instance variables for each config.ini parameter
-        """
-        self.data_directory = None
-        self.database_path = None
-        self.graph_directory = None
-        self.integrity_path = None
-        self.camera_drive = None
-        self.backup_drive = None
-        self.environment_logging = None
-        self.camera_logging = None
-        self.statistic_generation = None
-        self.day_graph_generation = None
-        self.month_graph_generation = None
-        self.year_graph_generation = None
-        self.report_uploading = None
-        self.environment_uploading = None
-        self.statistic_uploading = None
-        self.camera_uploading = None
-        self.day_graph_uploading = None
-        self.month_graph_uploading = None
-        self.year_graph_uploading = None
-        self.integrity_checks = None
-        self.local_network_server = None
-        self.backups = None
-        self.icaws_identifier = None
-        self.icaws_name = None
-        self.icaws_location = None
-        self.icaws_time_zone = None
-        self.icaws_latitude = None
-        self.icaws_longitude = None
-        self.icaws_elevation = None
-        self.remote_sql_server = None
-        self.remote_ftp_server = None
-        self.remote_ftp_username = None
-        self.remote_ftp_password = None
+    self.data_directory = None
+    self.database_path = None
+    self.graph_directory = None
+    self.integrity_path = None
+    self.camera_drive = None
+    self.backup_drive = None
+    self.environment_logging = None
+    self.camera_logging = None
+    self.statistic_generation = None
+    self.day_graph_generation = None
+    self.month_graph_generation = None
+    self.year_graph_generation = None
+    self.report_uploading = None
+    self.environment_uploading = None
+    self.statistic_uploading = None
+    self.camera_uploading = None
+    self.day_graph_uploading = None
+    self.month_graph_uploading = None
+    self.year_graph_uploading = None
+    self.integrity_checks = None
+    self.local_network_server = None
+    self.backups = None
+    self.icaws_identifier = None
+    self.icaws_name = None
+    self.icaws_location = None
+    self.icaws_time_zone = None
+    self.icaws_latitude = None
+    self.icaws_longitude = None
+    self.icaws_elevation = None
+    self.remote_sql_server = None
+    self.remote_ftp_server = None
+    self.remote_ftp_username = None
+    self.remote_ftp_password = None
 
     def load(self):
         """ Loads data from the config.ini file in the current directory
