63ec5f3ac5f81de2b89f00cf51bf4f1c1e5e8953
Henry Hunt <henryhunt.dev@gmail.com>
2018-03-31 15:27:02 +0000

    Code formatting

--- analysis.py
+++ analysis.py
@@ -2,9 +2,9 @@ import sqlite3
 
 from frames import DbTable
 
-def record_for_time(config, utc, table):
+def record_for_time(config, time, table):
     try:
-        utc = utc.replace(second = 0, microsecond = 0)
+        time = utc.replace(second = 0, microsecond = 0)
         
         with sqlite3.connect(config.database_path) as database:
             database.row_factory = sqlite3.Row
@@ -12,11 +12,14 @@ def record_for_time(config, utc, table):
 
             # Query respective database table
             if table == DbTable.UTCREPORTS:
-                cursor.execute("SELECT * FROM utcReports WHERE Time = ?", (utc.strftime("%Y-%m-%d %H:%M:%S"),))
+                cursor.execute("SELECT * FROM utcReports WHERE Time = ?",
+                               (time.strftime("%Y-%m-%d %H:%M:%S"),))
             elif table == DbTable.UTCENVIRON:
-                cursor.execute("SELECT * FROM utcEnviron WHERE Time = ?", (utc.strftime("%Y-%m-%d %H:%M:%S"),))
+                cursor.execute("SELECT * FROM utcEnviron WHERE Time = ?",
+                               (time.strftime("%Y-%m-%d %H:%M:%S"),))
             elif table == DbTable.LOCALSTATS:
-                cursor.execute("SELECT * FROM localStats WHERE Date = ?", (utc.strftime("%Y-%m-%d"),))
+                cursor.execute("SELECT * FROM localStats WHERE Date = ?",
+                               (time.strftime("%Y-%m-%d"),))
 
             return cursor.fetchone()
     except: return False
