d379baa7a3f591a1143ae926b4417f88d74998f0
Henry Hunt <henryhunt.dev@gmail.com>
2018-03-31 15:08:33 +0100

    Added analysis.py and record_for_time() function

---
+++ analysis.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+from frames import DbTable
+
+def record_for_time(config, utc, table)
+    try:
+        with sqlite3.connect(config.database_path) as database:
+            database.row_factory = sqlite3.Row
+            cursor = database.cursor()
+
+            # Query respective database table
+            if table == DbTable.UTCREPORTS:
+                cursor.execute("SELECT * FROM utcReports WHERE utc = ?", (utc.strftime("%Y-%m-%d %H:%M:%S"),))
+            elif table == DbTable.UTCENVIRON:
+                cursor.execute("SELECT * FROM utcEnviron WHERE utc = ?", (utc.strftime("%Y-%m-%d %H:%M:%S"),))
+            elif table == DbTable.LOCALSTATS:
+                cursor.execute("SELECT * FROM localStats WHERE utc = ?", (utc.strftime("%Y-%m-%d"),))
+
+            return cursor.fetchone()
+    except: return False
