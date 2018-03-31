12d0fcd20aeb95c91c18141a69d1abf308e1e80c
Henry Hunt <henryhunt.dev@gmail.com>
2018-03-31 21:01:52 +0100

    Code formatting

--- icaws_data.py
+++ icaws_data.py
@@ -202,35 +202,35 @@ if not os.path.isfile(config.database_path):
     try:
         with sqlite3.connect(config.database_path) as database:
             cursor = database.cursor()
-            cursor.execute("CREATE TABLE utcReports (" +
-                                "Time TEXT PRIMARY KEY NOT NULL," +
-                                "AirT REAL, ExpT REAL, RelH REAL," +
-                                "DewP REAL, WSpd REAL, WDir REAL," +
-                                "WGst REAL, SunD REAL, Rain REAL," +
-                                "StaP REAL, PTen REAL, MSLP REAL," +
-                                "ST10 REAL, ST30 REAL, ST00 REAL" +
-                            ")")
-            cursor.execute("CREATE TABLE utcEnviron (" +
-                                "Time TEXT PRIMARY KEY NOT NULL," +
-                                "EncT REAL, CPUT REAL" +
-                            ")")
-            cursor.execute("CREATE TABLE localStats (" +
-                                "Date TEXT PRIMARY KEY NOT NULL," +
-                                "AirT_Min REAL, AirT_Max REAL," +
-                                "AirT_Avg REAL, RelH_Min REAL," +
-                                "RelH_Max REAL, RelH_Avg REAL," +
-                                "DewP_Min REAL, DewP_Max REAL," +
-                                "DewP_Avg REAL, WSpd_Max REAL," +
-                                "WSpd_Avg REAL, WDir_Avg REAL," +
-                                "WGst_Max REAL, WGst_Avg REAL," +
-                                "SunD_Ttl REAL, Rain_Ttl REAL," +
-                                "MSLP_Min REAL, MSLP_Max REAL," +
-                                "MSLP_Avg REAL, TS10_Min REAL," +
-                                "TS10_Max REAL, TS10_Avg REAL," +
-                                "TS30_Min REAL, TS30_Max REAL," +
-                                "TS30_Avg REAL, TS00_Min REAL," +
-                                "TS00_Max REAL, TS00_Avg REAL" +
-                            ")")
+            cursor.execute("CREATE TABLE utcReports ("
+                                + "Time TEXT PRIMARY KEY NOT NULL,"
+                                + "AirT REAL, ExpT REAL, RelH REAL,"
+                                + "DewP REAL, WSpd REAL, WDir REAL,"
+                                + "WGst REAL, SunD REAL, Rain REAL,"
+                                + "StaP REAL, PTen REAL, MSLP REAL,"
+                                + "ST10 REAL, ST30 REAL, ST00 REAL"
+                            + ")")
+            cursor.execute("CREATE TABLE utcEnviron ("
+                                + "Time TEXT PRIMARY KEY NOT NULL,"
+                                + "EncT REAL, CPUT REAL"
+                            + ")")
+            cursor.execute("CREATE TABLE localStats ("
+                                + "Date TEXT PRIMARY KEY NOT NULL,"
+                                + "AirT_Min REAL, AirT_Max REAL,"
+                                + "AirT_Avg REAL, RelH_Min REAL,"
+                                + "RelH_Max REAL, RelH_Avg REAL,"
+                                + "DewP_Min REAL, DewP_Max REAL,"
+                                + "DewP_Avg REAL, WSpd_Max REAL,"
+                                + "WSpd_Avg REAL, WDir_Avg REAL,"
+                                + "WGst_Max REAL, WGst_Avg REAL,"
+                                + "SunD_Ttl REAL, Rain_Ttl REAL,"
+                                + "MSLP_Min REAL, MSLP_Max REAL,"
+                                + "MSLP_Avg REAL, TS10_Min REAL,"
+                                + "TS10_Max REAL, TS10_Avg REAL,"
+                                + "TS30_Min REAL, TS30_Max REAL,"
+                                + "TS30_Avg REAL, TS00_Min REAL,"
+                                + "TS00_Max REAL, TS00_Avg REAL"
+                            + ")")
             database.commit()
     except: helpers.exit("05")
 
