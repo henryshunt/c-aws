c387f015962eadde58797101e4aabea089a6dba5
Henry Hunt <henryhunt.dev@gmail.com>
2018-04-01 15:49:05 +0100

    day_bounds_utc() bug fixes

--- helpers.py
+++ helpers.py
@@ -56,13 +56,40 @@ def local_to_utc(config, local):
     localised = config.icaws_time_zone.localize(local)
     return (localised.astimezone(pytz.utc).replace(tzinfo = None))
 
+# def day_bounds_utc(config, local, inclusive):
+#     # Get start and end of local day
+#     start = local.replace(hour = 0, minute = 0, second = 0, microsecond = 0)
+#     end = local.replace(hour = 23, minute = 59, second = 0, microsecond = 0)
+
+#     # Use start of next day as end if inclusive
+#     if inclusive == True: end = local + timedelta(minute = 1)
+
+#     # Convert start and end to utc
+#     return local_to_utc(config, start), local_to_utc(config, end)
+
+
 def day_bounds_utc(config, local, inclusive):
-    # Get start and end of local day
-    start = local.replace(hour = 0, minute = 0, second = 0, microsecond = 0)
-    end = local.replace(hour = 23, minute = 59, second = 0, microsecond = 0)
+    """ returns the utc start and end times of a date in the local timezone
+    """
+    utc_timezone = pytz.timezone("UTC")
 
-    # Use start of next day as end if inclusive
-    if inclusive == True: end = local + timedelta(minute = 1)
+    # set local time to start of day and convert to utc
+    start = local.replace(hour = 0, minute = 0, second = 0, microsecond = 0,
+                          tzinfo = None)
+    start = pytz.timezone(config.icaws_time_zone).localize(start)
+    start_utc = start.astimezone(utc_timezone).replace(tzinfo = None)
 
-    # Convert start and end to utc
-    return local_to_utc(config, start), local_to_utc(config, end)
+    # set local time to end of day
+    end = local.replace(hour = 23, minute = 59, second = 0, microsecond = 0,
+                        tzinfo = None)
+
+    # use start of next day as end if inclusive
+    if inclusive == True:
+        end = local + timedelta(minutes = 1)
+        end = end.replace(hour = 0, minute = 0, second = 0, microsecond = 0,
+                          tzinfo = None)
+
+    # localise end and convert to utc
+    end = pytz.timezone(config.icaws_time_zone).localize(end)
+    end_utc = end.astimezone(utc_timezone).replace(tzinfo = None)
+    return start_utc, end_utc
