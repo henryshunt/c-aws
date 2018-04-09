b0ad9fd3106385bbd25ac2c66140eb6f10242635
Henry Hunt <henryhunt.dev@gmail.com>
2018-04-09 15:54:07 +0100

    Moved camera capture code into a with statement

--- CAWS_Dat.py
+++ CAWS_Dat.py
@@ -353,16 +353,15 @@ def do_log_camera(utc):
             image_name = utc.strftime("%Y-%m-%dT%H-%M-%S")
         
             # Set image annotation and capture image
-            camera = picamera.PiCamera()
-            camera.annotate_background = picamera.Color("black")
-            camera.annotate_text_size = 28
-            time.sleep(1)
+            with picamera.PiCamera() as camera:
+                camera.annotate_background = picamera.Color("black")
+                camera.annotate_text_size = 28
+                time.sleep(1)
 
-            local_time = helpers.utc_to_local(config, utc)
-            camera.annotate_text = ("CAWS Camera " + local_time.strftime(
-                                    "on %d/%m/%Y at %H:%M:%S"))
-            camera.capture(os.path.join(image_dir, image_name + ".jpg"))
-            camera.close()
+                local_time = helpers.utc_to_local(config, utc)
+                camera.annotate_text = ("CAWS Camera " + local_time.strftime(
+                                        "on %d/%m/%Y at %H:%M:%S"))
+                camera.capture(os.path.join(image_dir, image_name + ".jpg"))
         except: gpio.output(23, gpio.HIGH)
 
 def do_generate_stats(utc):
