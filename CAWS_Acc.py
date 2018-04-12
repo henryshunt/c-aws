fa2117e12f5219e2c8c7b675a232ba59d71016fd
Henry Hunt <henryhunt.dev@gmail.com>
2018-04-12 15:44:34 +0100

    Started implementing local server

--- CAWS_Acc.py
+++ CAWS_Acc.py
@@ -5,6 +5,9 @@
 
 # DEPENDENCIES -----------------------------------------------------------------
 import time
+from datetime import datetime, timedelta
+
+import flask
 
 # MESSAGE ----------------------------------------------------------------------
 print("--- Custom Automatic Weather Station ---")
@@ -13,3 +16,48 @@ print("Author:  Henry Hunt")
 print("Version: V4.0 (April 2018)")
 print("")
 print("----------- DO NOT TERMINATE -----------")
+
+# GLOBAL VARIABLES -------------------------------------------------------------
+start_time = None
+
+# PAGE SERVERS -----------------------------------------------------------------
+def page_now():
+      return "Now"
+
+def page_statistics():
+      return "Statistics"
+
+def page_graph_day():
+      return "Graph Day"
+
+def page_graph_month():
+      return "Graph Month"
+
+def page_graph_year():
+      return "Graph Year"
+
+def page_camera():
+      return "Camera"
+
+def page_about():
+      return "About"
+
+
+# ENTRY POINT ==================================================================
+# -- CREATE SERVER -------------------------------------------------------------
+server = flask.Flask(__name__, static_folder = "interface",
+                     template_folder = "interface")
+
+# -- ROUTE URLS ----------------------------------------------------------------
+server.add_url_rule("/", "", page_now)
+server.add_url_rule("/", "index.html", page_now)
+server.add_url_rule("/", "statistics.html", page_statistics)
+server.add_url_rule("/", "graph_day.html", page_graph_day)
+server.add_url_rule("/", "graph_month.html", page_graph_month)
+server.add_url_rule("/", "graph_year.html", page_graph_year)
+server.add_url_rule("/", "camera.html", page_camera)
+server.add_url_rule("/", "about.html", page_about)
+
+# -- START SERVER --------------------------------------------------------------
+start_time = datetime.utcnow().replace(second = 0, microsecond = 0)
+server.run(host = "0.0.0.0", port = 80, threaded = True)
