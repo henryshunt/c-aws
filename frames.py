de0c3d3edde686c9765586ff0084fb1ea9922481
Henry Hunt <henryhunt.dev@gmail.com>
2018-04-12 15:29:21 +0100

    DataLocalStat not needed, removed

--- frames.py
+++ frames.py
@@ -25,42 +25,6 @@ class DataUtcEnviron():
         self.enclosure_temperature = None
         self.cpu_temperature = None
 
-class DataLocalStat():
-    def __init__(self):
-        self.date = None
-        self.air_temperature_min = None
-        self.air_temperature_max = None
-        self.air_temperature_avg = None
-        self.relative_humidity_min = None
-        self.relative_humidity_max = None
-        self.relative_humidity_avg = None
-        self.dew_point_min = None
-        self.dew_point_max = None
-        self.dew_point_avg = None
-        self.wind_speed_min = None
-        self.wind_speed_max = None
-        self.wind_speed_avg = None
-        self.wind_direction_min = None
-        self.wind_direction_max = None
-        self.wind_direction_avg = None
-        self.wind_gust_min = None
-        self.wind_gust_max = None
-        self.wind_gust_avg = None
-        self.sunshine_duration_ttl = None
-        self.rainfall_ttl = None
-        self.mean_sea_level_pressure_min = None
-        self.mean_sea_level_pressure_max = None
-        self.mean_sea_level_pressure_avg = None
-        self.soil_temperature_10_min = None
-        self.soil_temperature_10_max = None
-        self.soil_temperature_10_avg = None
-        self.soil_temperature_30_min = None
-        self.soil_temperature_30_max = None
-        self.soil_temperature_30_avg = None
-        self.soil_temperature_00_min = None
-        self.soil_temperature_00_max = None
-        self.soil_temperature_00_avg = None
-
 class DbTable(Enum):
     UTCREPORTS = 1
     UTCENVIRON = 2
