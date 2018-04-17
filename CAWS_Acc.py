a7fa452c38240ddfe94c1b68f2a80cc127fca420
Henry Hunt <henryhunt.dev@gmail.com>
2018-04-17 13:25:18 +0100

    Finished implementing Today graph page server

--- CAWS_Acc.py
+++ CAWS_Acc.py
@@ -243,19 +243,50 @@ def page_statistics():
                                  override_update = override_update)
 
 def page_graph_day():
-    bounds = helpers.day_bounds_utc(config, helpers.utc_to_local(
-        config, datetime.utcnow()), True)
+    utc = datetime.utcnow(); utc_second = utc.second
+    utc = utc.replace(second = 0, microsecond = 0)
 
+    # Check for a local date in the URL
+    if flask.request.args.get("date") != None:
+        try:
+            url_date = datetime.strptime(
+                flask.request.args.get("date"), "%Y-%m-%d")
+            local_date = helpers.utc_to_local(config, utc)
+
+            # Check if date in URL is the same as current date
+            if (url_date.strftime("%Y-%m-%d")
+                == local_date.strftime("%Y-%m-%d")):
+
+                return flask.redirect(flask.url_for("page_graph_day"))
+
+            else:
+                utc = helpers.local_to_utc(config, url_date)
+                local_time = url_date
+                bounds = helpers.day_bounds_utc(config, helpers.utc_to_local(
+                                                config, utc), True)
+        except: return flask.redirect(flask.url_for("page_graph_day"))
+
+    else:
+        local_time = helpers.utc_to_local(config, utc)
+        bounds = helpers.day_bounds_utc(config, local_time, True)
+    
     start = bounds[0].strftime("%Y-%m-%dT%H-%M-%S")
     end = bounds[1].strftime("%Y-%m-%dT%H-%M-%S")
     low = helpers.utc_to_local(config, bounds[0]).timestamp()
     high = helpers.utc_to_local(config, bounds[1]).timestamp()
 
+    scroller_prev = (local_time - timedelta(days = 1)).strftime("%Y-%m-%d")
+    scroller_time = local_time.strftime("%d/%m/%Y")
+    scroller_next = (local_time + timedelta(days = 1)).strftime("%Y-%m-%d")
+
     return flask.render_template("graph_day.html",
                                  caws_name = config.caws_name,
                                  caws_location = config.caws_location,
                                  start = start, end = end,
-                                 low = low, high = high)
+                                 low = low, high = high,
+                                 scroller_prev = scroller_prev,
+                                 scroller_time = scroller_time,
+                                 scroller_next = scroller_next)
 
 def page_graph_month():
     return flask.render_template("graph_month.html",
