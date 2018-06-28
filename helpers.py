6e60861bf7b52aa0b5decd66d14fa64500846def
Henry Hunt <henryhunt.dev@gmail.com>
2018-06-28 15:39:57 +0100

    Refactoring, removed init_success()

--- helpers.py
+++ helpers.py
@@ -6,6 +6,28 @@ import pytz
 
 from config import ConfigData
 
+def init_exit(code):
+    try:
+        with open("init.txt", "w+") as file:
+            file.write("initialisation failure at {} UTC with exit code {}"
+                       .format(datetime.utcnow()
+                               .strftime("%Y-%m-%dT%H:%M:%S"), code))
+    except: pass
+
+    gpio.output(23, gpio.HIGH)
+    sys.exit(1)
+
+def init_exit_blind(code):
+    try:
+        with open("init.txt", "w+") as file:
+            file.write("initialisation failure at {} UTC with exit code {}"
+                       .format(datetime.utcnow()
+                               .strftime("%Y-%m-%dT%H:%M:%S"), code))
+    except: pass
+
+    sys.exit(1)
+
+
 def remaining_space(directory):
     """ Returns the amount of remaining space in gigabytes, for non-root users,
         for the specified directory to a device
@@ -17,36 +39,6 @@ def remaining_space(directory):
         non_root_space = float(disk.f_bsize * disk.f_bavail)
         return non_root_space / 1024 / 1024 / 1024
     except: return None
-
-def exit(code):
-    try:
-        with open("init.txt", "w+") as file:
-            file.write("initialisation failure at {} UTC with exit code {}"
-                       .format(datetime.utcnow()
-                               .strftime("%Y-%m-%dT%H:%M:%S"), code))
-    except: pass
-
-    gpio.output(23, gpio.HIGH)
-    sys.exit(1)
-
-def exit_no_light(code):
-    try:
-        with open("init.txt", "w+") as file:
-            file.write("initialisation failure at {} UTC with exit code {}"
-                       .format(datetime.utcnow()
-                               .strftime("%Y-%m-%dT%H:%M:%S"), code))
-    except: pass
-    sys.exit(1)
-
-def init_success():
-    try:
-        with open("init.txt", "w+") as file:
-            file.write("initialisation success at {} UTC"
-                       .format(datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S")))
-
-    except:
-        gpio.output(23, gpio.HIGH)
-        sys.exit(1)
 
 def utc_to_local(config, utc):
     localised = pytz.utc.localize(utc.replace(tzinfo = None))
