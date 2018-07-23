9f0485f9f970f3a0a37208bac6bc86036448d154
Henry Hunt <henryhunt.dev@gmail.com>
2018-07-23 16:20:35 +0100

    Minor code refinements

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """ C-AWS Main Entry Point
-      Performs initial environment checks and starts the sub-systems
+      Performs the initial environment checks and starts the sub-systems
 """
 
 # DEPENDENCIES -----------------------------------------------------------------
@@ -29,7 +29,8 @@ config = ConfigData()
 startup_time = datetime.utcnow()
 
 # CLEAR STATE ------------------------------------------------------------------
-try: os.remove("__init.txt")
+try:
+    os.remove("__init.txt")
 except: print("state clear failure")
 
 # -- INIT GPIO AND LEDS --------------------------------------------------------
@@ -37,25 +38,25 @@ try:
     gpio.setwarnings(False); gpio.setmode(gpio.BCM)
     gpio.setup(23, gpio.OUT); gpio.output(23, gpio.LOW)
     gpio.setup(24, gpio.OUT); gpio.output(24, gpio.LOW)
-except: helpers.init_exit_blind("00")
+except: helpers.init_exit("00", False)
 
 gpio.output(23, gpio.HIGH); gpio.output(24, gpio.HIGH)
 time.sleep(2.5)
 gpio.output(23, gpio.LOW); gpio.output(24, gpio.LOW)
 
 # -- CHECK CONFIG --------------------------------------------------------------
-if config.load() == False: helpers.init_exit("01")
-if config.validate() == False: helpers.init_exit("02")
+if config.load() == False: helpers.init_exit("01", True)
+if config.validate() == False: helpers.init_exit("02", True)
 
 # -- CHECK INTERNAL DRIVE ------------------------------------------------------
 free_space = helpers.remaining_space("/")
-if free_space == None or free_space < 1: helpers.init_exit("03")
+if free_space == None or free_space < 1: helpers.init_exit("03", True)
 
 # -- CHECK DATA DIRECTORY ------------------------------------------------------
 if not os.path.isdir(config.data_directory):
     try:
         os.makedirs(config.data_directory)
-    except: helpers.init_exit("04")
+    except: helpers.init_exit("04", True)
 
 # -- MAKE DATABASE -------------------------------------------------------------
 if not os.path.isfile(config.database_path):
@@ -67,14 +68,14 @@ if not os.path.isfile(config.database_path):
             cursor.execute(queries.CREATE_DAYSTATS_TABLE)
 
             database.commit()
-    except: helpers.init_exit("05")
+    except: helpers.init_exit("05", True)
 
 # -- CHECK CAMERA DRIVE --------------------------------------------------------
 if config.camera_logging == True:
-    if not os.path.isdir(config.camera_drive): helpers.init_exit("06")
+    if not os.path.isdir(config.camera_drive): helpers.init_exit("06", True)
 
     free_space = helpers.remaining_space(config.camera_drive)
-    if free_space == None or free_space < 5: helpers.init_exit("07")
+    if free_space == None or free_space < 5: helpers.init_exit("07", True)
 
     # Check camera module is connected
     try:
@@ -86,7 +87,7 @@ try:
     subprocess.Popen(["lxterminal -e python3 aws_access.py"
                       + startup_time.strftime(" %Y-%m-%dT%H:%M:%S")],
                      shell = True)
-except: helpers.init_exit("09")
+except: helpers.init_exit("09", True)
 
 if (config.reports_uploading == True or
     config.envReports_uploading == True or
@@ -95,8 +96,8 @@ if (config.reports_uploading == True or
 
     try:
         subprocess.Popen(["lxterminal -e python3 aws_support.py"], shell = True)
-    except: helpers.init_exit("10")
+    except: helpers.init_exit("10", True)
 
 try:
     subprocess.Popen(["lxterminal -e python3 aws_data.py"], shell = True)
-except: helpers.init_exit("11")
+except: helpers.init_exit("11", True)
