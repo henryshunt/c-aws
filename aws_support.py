a81473dda2a26a8e71d486586e29f3b6794583c6
Henry Hunt <henryhunt.dev@gmail.com>
2018-07-27 00:06:07 +0100

    Re-written most of the access subsystem

--- aws_support.py
+++ aws_support.py
@@ -7,6 +7,8 @@ from datetime import datetime
 import os
 import threading
 import requests
+from collections import deque
+import ftplib
 
 import daemon
 from apscheduler.schedulers.blocking import BlockingScheduler
@@ -19,140 +21,152 @@ from config import ConfigData
 # GLOBAL VARIABLES -------------------------------------------------------------
 config = ConfigData()
 
-is_processing_data = False
-is_processing_camera = True
-data_queue = []
-camera_queue = []
+data_queue = deque()
+camera_queue = deque()
 
 # OPERATIONS -------------------------------------------------------------------
-def do_upload_data(data):
-    global data_queue, is_processing_data
-    try: data_queue.append(data)
-    except: pass
+def do_process_data_queue():
+    global data_queue
 
-    # Only process queue if not already being processed
-    if is_processing_data == False and len(data_queue) > 0:
-        threading.Thread(target = do_process_data).start()
+    # Process while there are items in the queue
+    while len(data_queue > 0):
+        try: data = data_queue.popleft()
+        except: return
 
-def do_upload_camera(image_path):
-    global camera_queue, is_processing_camera
-    try: camera_queue.append(image_path)
-    except: pass
+        # Generate record status booleans
+        has_report = False if data[0] == False or data[0] == None else True
+        has_envReport = False if data[1] == False or data[1] == None else True
+        has_dayStat = False if data[2] == False or data[2] == None else True
 
-    # Only process queue if not already being processed
-    if not is_processing_camera and len(camera_queue) > 0:
-        threading.Thread(target = do_process_camera).start()
-
-def do_process_data():
-    global data_queue, camera_queue, is_processing_data, is_processing_camera
-    is_processing_data = True
-
-    for data in list(data_queue):
-        if data[0] == False or data[0] == None: data_has_report = 0
-        else: data_has_report = 1
-        if data[1] == False or data[1] == None: data_has_stat = 0
-        else: data_has_stat = 1
-        if data[2] == False or data[2] == None: data_has_environ = 0
-        else: data_has_environ = 1
-
-        data_new = {
-            "pass": "caws-warwick",
-
-            "has_report": data_has_report,
-            "has_stat": data_has_stat,
-            "has_environ": data_has_environ
+        to_upload = {
+            "has_report": 1 if has_report == True else 0,
+            "has_envReport": 1 if has_envReport == True else 0,
+            "has_dayStat": 1 if has_dayStat == True else 0
         }
 
-        if data_has_report == 1:
-            data_new["RTime"] = helpers.none_to_null(data[0]["Time"]).replace(" ", "+"),
-            data_new["AirT"] = helpers.none_to_null(data[0]["AirT"]),
-            data_new["ExpT"] = helpers.none_to_null(data[0]["ExpT"]),
-            data_new["RelH"] = helpers.none_to_null(data[0]["RelH"]),
-            data_new["DewP"] = helpers.none_to_null(data[0]["DewP"]),
-            data_new["WSpd"] = helpers.none_to_null(data[0]["WSpd"]),
-            data_new["WDir"] = helpers.none_to_null(data[0]["WDir"]),
-            data_new["WGst"] = helpers.none_to_null(data[0]["WGst"]),
-            data_new["SunD"] = helpers.none_to_null(data[0]["SunD"]),
-            data_new["Rain"] = helpers.none_to_null(data[0]["Rain"]),
-            data_new["StaP"] = helpers.none_to_null(data[0]["StaP"]),
-            data_new["PTen"] = helpers.none_to_null(data[0]["PTen"]),
-            data_new["MSLP"] = helpers.none_to_null(data[0]["MSLP"]),
-            data_new["ST10"] = helpers.none_to_null(data[0]["ST10"]),
-            data_new["ST30"] = helpers.none_to_null(data[0]["ST30"]),
-            data_new["ST00"] = helpers.none_to_null(data[0]["ST00"]),
+        # Add report data to data to upload
+        if has_report == True:
+            to_upload["Time"] = data[0]["Time"],
+            to_upload["AirT"] = helpers.none_to_null(data[0]["AirT"]),
+            to_upload["ExpT"] = helpers.none_to_null(data[0]["ExpT"]),
+            to_upload["RelH"] = helpers.none_to_null(data[0]["RelH"]),
+            to_upload["DewP"] = helpers.none_to_null(data[0]["DewP"]),
+            to_upload["WSpd"] = helpers.none_to_null(data[0]["WSpd"]),
+            to_upload["WDir"] = helpers.none_to_null(data[0]["WDir"]),
+            to_upload["WGst"] = helpers.none_to_null(data[0]["WGst"]),
+            to_upload["SunD"] = helpers.none_to_null(data[0]["SunD"]),
+            to_upload["Rain"] = helpers.none_to_null(data[0]["Rain"]),
+            to_upload["StaP"] = helpers.none_to_null(data[0]["StaP"]),
+            to_upload["MSLP"] = helpers.none_to_null(data[0]["MSLP"]),
+            to_upload["ST10"] = helpers.none_to_null(data[0]["ST10"]),
+            to_upload["ST30"] = helpers.none_to_null(data[0]["ST30"]),
+            to_upload["ST00"] = helpers.none_to_null(data[0]["ST00"])
 
-        if data_has_stat == 1:
-            data_new["Date"] = helpers.none_to_null(data[1]["Date"]),
-            data_new["AirT_Min"] = helpers.none_to_null(data[1]["AirT_Min"]),
-            data_new["AirT_Max"] = helpers.none_to_null(data[1]["AirT_Max"]),
-            data_new["AirT_Avg"] = helpers.none_to_null(data[1]["AirT_Avg"]),
-            data_new["RelH_Min"] = helpers.none_to_null(data[1]["RelH_Min"]),
-            data_new["RelH_Max"] = helpers.none_to_null(data[1]["RelH_Max"]),
-            data_new["RelH_Avg"] = helpers.none_to_null(data[1]["RelH_Avg"]),
-            data_new["DewP_Min"] = helpers.none_to_null(data[1]["DewP_Min"]),
-            data_new["DewP_Max"] = helpers.none_to_null(data[1]["DewP_Max"]),
-            data_new["DewP_Avg"] = helpers.none_to_null(data[1]["DewP_Avg"]),
-            data_new["WSpd_Min"] = helpers.none_to_null(data[1]["WSpd_Min"]),
-            data_new["WSpd_Max"] = helpers.none_to_null(data[1]["WSpd_Max"]),
-            data_new["WSpd_Avg"] = helpers.none_to_null(data[1]["WSpd_Avg"]),
-            data_new["WDir_Min"] = helpers.none_to_null(data[1]["WDir_Min"]),
-            data_new["WDir_Max"] = helpers.none_to_null(data[1]["WDir_Max"]),
-            data_new["WDir_Avg"] = helpers.none_to_null(data[1]["WDir_Avg"]),
-            data_new["WGst_Min"] = helpers.none_to_null(data[1]["WGst_Min"]),
-            data_new["WGst_Max"] = helpers.none_to_null(data[1]["WGst_Max"]),
-            data_new["WGst_Avg"] = helpers.none_to_null(data[1]["WGst_Avg"]),
-            data_new["SunD_Ttl"] = helpers.none_to_null(data[1]["SunD_Ttl"]),
-            data_new["Rain_Ttl"] = helpers.none_to_null(data[1]["Rain_Ttl"]),
-            data_new["MSLP_Min"] = helpers.none_to_null(data[1]["MSLP_Min"]),
-            data_new["MSLP_Max"] = helpers.none_to_null(data[1]["MSLP_Max"]),
-            data_new["MSLP_Avg"] = helpers.none_to_null(data[1]["MSLP_Avg"]),
-            data_new["ST10_Min"] = helpers.none_to_null(data[1]["ST10_Min"]),
-            data_new["ST10_Max"] = helpers.none_to_null(data[1]["ST10_Max"]),
-            data_new["ST10_Avg"] = helpers.none_to_null(data[1]["ST10_Avg"]),
-            data_new["ST30_Min"] = helpers.none_to_null(data[1]["ST30_Min"]),
-            data_new["ST30_Max"] = helpers.none_to_null(data[1]["ST30_Max"]),
-            data_new["ST30_Avg"] = helpers.none_to_null(data[1]["ST30_Avg"]),
-            data_new["ST00_Min"] = helpers.none_to_null(data[1]["ST00_Min"]),
-            data_new["ST00_Max"] = helpers.none_to_null(data[1]["ST00_Max"]),
-            data_new["ST00_Avg"] = helpers.none_to_null(data[1]["ST00_Avg"]),
+        # Add environment data to data to upload
+        if has_envReport == True:
+            to_upload["EncT"] = helpers.none_to_null(data[1]["EncT"]),
+            to_upload["CPUT"] = helpers.none_to_null(data[1]["CPUT"])
 
-        if data_has_environ == 1:
-            data_new["ETime"] = helpers.none_to_null(data[2]["Time"]).replace(" ", "+"),
-            data_new["EncT"] = helpers.none_to_null(data[2]["EncT"]),
-            data_new["CPUT"] = helpers.none_to_null(data[2]["CPUT"])
+        # Add statistic data to data to upload
+        if has_dayStat == True:
+            to_upload["Date"] = data[2]["Date"],
+            to_upload["AirT_Min"] = helpers.none_to_null(data[2]["AirT_Min"]),
+            to_upload["AirT_Max"] = helpers.none_to_null(data[2]["AirT_Max"]),
+            to_upload["AirT_Avg"] = helpers.none_to_null(data[2]["AirT_Avg"]),
+            to_upload["RelH_Min"] = helpers.none_to_null(data[2]["RelH_Min"]),
+            to_upload["RelH_Max"] = helpers.none_to_null(data[2]["RelH_Max"]),
+            to_upload["RelH_Avg"] = helpers.none_to_null(data[2]["RelH_Avg"]),
+            to_upload["DewP_Min"] = helpers.none_to_null(data[2]["DewP_Min"]),
+            to_upload["DewP_Max"] = helpers.none_to_null(data[2]["DewP_Max"]),
+            to_upload["DewP_Avg"] = helpers.none_to_null(data[2]["DewP_Avg"]),
+            to_upload["WSpd_Min"] = helpers.none_to_null(data[2]["WSpd_Min"]),
+            to_upload["WSpd_Max"] = helpers.none_to_null(data[2]["WSpd_Max"]),
+            to_upload["WSpd_Avg"] = helpers.none_to_null(data[2]["WSpd_Avg"]),
+            to_upload["WDir_Min"] = helpers.none_to_null(data[2]["WDir_Min"]),
+            to_upload["WDir_Max"] = helpers.none_to_null(data[2]["WDir_Max"]),
+            to_upload["WDir_Avg"] = helpers.none_to_null(data[2]["WDir_Avg"]),
+            to_upload["WGst_Min"] = helpers.none_to_null(data[2]["WGst_Min"]),
+            to_upload["WGst_Max"] = helpers.none_to_null(data[2]["WGst_Max"]),
+            to_upload["WGst_Avg"] = helpers.none_to_null(data[2]["WGst_Avg"]),
+            to_upload["SunD_Ttl"] = helpers.none_to_null(data[2]["SunD_Ttl"]),
+            to_upload["Rain_Ttl"] = helpers.none_to_null(data[2]["Rain_Ttl"]),
+            to_upload["MSLP_Min"] = helpers.none_to_null(data[2]["MSLP_Min"]),
+            to_upload["MSLP_Max"] = helpers.none_to_null(data[2]["MSLP_Max"]),
+            to_upload["MSLP_Avg"] = helpers.none_to_null(data[2]["MSLP_Avg"]),
+            to_upload["ST10_Min"] = helpers.none_to_null(data[2]["ST10_Min"]),
+            to_upload["ST10_Max"] = helpers.none_to_null(data[2]["ST10_Max"]),
+            to_upload["ST10_Avg"] = helpers.none_to_null(data[2]["ST10_Avg"]),
+            to_upload["ST30_Min"] = helpers.none_to_null(data[2]["ST30_Min"]),
+            to_upload["ST30_Max"] = helpers.none_to_null(data[2]["ST30_Max"]),
+            to_upload["ST30_Avg"] = helpers.none_to_null(data[2]["ST30_Avg"]),
+            to_upload["ST00_Min"] = helpers.none_to_null(data[2]["ST00_Min"]),
+            to_upload["ST00_Max"] = helpers.none_to_null(data[2]["ST00_Max"]),
+            to_upload["ST00_Avg"] = helpers.none_to_null(data[2]["ST00_Avg"])
 
+        # Upload the data
         try:
-            request = requests.post(config.remote_sql_server, data_new, timeout = 5)
+            request = requests.post(
+                config.remote_sql_server, to_upload, timeout = 8)
+            if request.text != "0": data_queue.appendleft(data); return
+        except: data_queue.appendleft(data); return
 
-            if request.text == "0": data_queue.remove(data)
-            else: break
-        except: break
+def do_process_camera_queue():
+    global camera_queue
 
-    is_processing_data = False
+    # Process while there are items in the queue
+    while len(camera_queue > 0):
+        try: data = camera_queue.popleft()
+        except: return
 
-def do_process_camera():
-    pass
+        # Upload image to camera folder on FTP server
+        if os.path.isfile(data):
+            try:
+                ftp_server = ftplib.FTP(config.remote_ftp_server,
+                    config.remote_ftp_username, config.remote_ftp_password,
+                    timeout = 45)
+                ftp_server.set_pasv(False)
+
+                with open(data, "rb") as file:
+                    ftp_server.storbinary("STOR data/camera/" + os.path, file)
+            except: return
+        else: return
 
 # SCHEDULERS -------------------------------------------------------------------
 def every_minute():
     utc = datetime.utcnow().replace(second = 0, microsecond = 0)
     local_time = helpers.utc_to_local(config, utc)
 
-    # Add data to upload queue if config modifiers are active
+    # Get report data if config modifier is active
     if config.reports_uploading == True:
         report = analysis.record_for_time(config, utc, DbTable.UTCREPORTS)
+
+        if report == False:
+            report = analysis.record_for_time(config, utc, DbTable.UTCREPORTS)
     else: report == None
 
+    # Get envReport data if config modifier is active
     if config.envReports_uploading == True:
         envReport = analysis.record_for_time(config, utc, DbTable.UTCENVIRON)
+
+        if envReport == False:
+            envReport = analysis.record_for_time(config, utc,
+                                                 DbTable.UTCENVIRON)
     else: envReport = None
 
+    # Get dayStat data is config modifier is active
     if config.dayStats_uploading == True:
         dayStat = analysis.record_for_time(config, local_time,
                                            DbTable.LOCALSTATS)
+
+        if dayStat == False:
+            dayStat = analysis.record_for_time(config, local_time,
+                                               DbTable.LOCALSTATS)
     else: dayStat = None
 
-    do_upload_data((report, envReport, dayStat))
+    # Add data to queue and process the queue
+    data_queue.append((report, envReport, dayStat))
+    do_process_data_queue()
+
 
     # Add camera image to upload queue if config modifier is active
     if config.camera_uploading == True:
@@ -161,7 +175,8 @@ def every_minute():
         if utc_minute.endswith("0") or utc_minute.endswith("5"):
             image_path = os.path.join(config.camera_drive,
                 utc.strftime("%Y/%m/%d/%Y-%m-%dT%H-%M-%S") + ".jpg")
-            if os.path.isfile(image_path): do_upload_camera(image_path)
+            camera_queue.append(image_path)
+            do_process_camera_queue()
     
 
 # ENTRY POINT ==================================================================
