583b8f52973051debc4b9834af91b192630006ed
Henry Hunt <henryhunt.dev@gmail.com>
2019-01-21 15:06:29 +0000

    Bug fixes

--- routines/config.py
+++ routines/config.py
@@ -64,80 +64,96 @@ class ConfigData():
             self.__parser.read("config.ini")
 
             # Load AWSInfo group values
-            self.aws_location = load_value(
-                "AWSInfo", "Location", DataType.STRING)
-            self.aws_time_zone = load_value(
-                "AWSInfo", "TimeZone", DataType.STRING)
-            self.aws_latitude = load_value(
-                "AWSInfo", "Latitude", DataType.FLOAT)
-            self.aws_longitude = load_value(
-                "AWSInfo", "Longitude", DataType.FLOAT)
-            self.aws_elevation = load_value(
-                "AWSInfo", "Elevation", DataType.FLOAT)
+            (self.aws_location
+                = self.load_value("AWSInfo", "Location", DataType.STRING))
+            (self.aws_time_zone
+                = self.load_value("AWSInfo", "TimeZone", DataType.STRING))
+            (self.aws_latitude
+                = self.load_value("AWSInfo", "Latitude", DataType.FLOAT))
+            (self.aws_longitude
+                = self.load_value("AWSInfo", "Longitude", DataType.FLOAT))
+            (self.aws_elevation
+                = self.load_value("AWSInfo", "Elevation", DataType.FLOAT))
 
             # Load DataStores group values
-            self.data_directory = load_value(
+            self.data_directory = self.load_value(
                 "DataStores", "DataDirectory", DataType.STRING)
-            self.camera_drive_label = load_value(
+            self.camera_drive_label = self.load_value(
                 "DataStores", "CameraDrive", DataType.STRING)
 
             # Load DataEndpoints group values
-            self.remote_sql_server = load_value(
+            self.remote_sql_server = self.load_value(
                 "DataEndpoints", "RemoteSQLServer", DataType.STRING)
-            self.remote_ftp_server = load_value(
+            self.remote_ftp_server = self.load_value(
                 "DataEndpoints", "RemoteFTPServer", DataType.STRING)
-            self.remote_ftp_username = load_value(
+            self.remote_ftp_username = self.load_value(
                 "DataEndpoints", "RemoteFTPUsername", DataType.STRING)
-            self.remote_ftp_password = load_value(
+            self.remote_ftp_password = self.load_value(
                 "DataEndpoints", "RemoteFTPPassword", DataType.STRING)
 
             # Load Operations group values
-            self.envReport_logging = load_value(
+            self.envReport_logging = self.load_value(
                 "Operations", "EnvReportLogging", DataType.BOOLEAN)
-            self.camera_logging = load_value(
+            self.camera_logging = self.load_value(
                 "Operations", "CameraLogging", DataType.BOOLEAN)
-            self.dayStat_generation = load_value(
+            self.dayStat_generation = self.load_value(
                 "Operations", "DayStatGeneration", DataType.BOOLEAN)
-            self.report_uploading = load_value(
+            self.report_uploading = self.load_value(
                 "Operations", "ReportUploading", DataType.BOOLEAN)
-            self.envReport_uploading = load_value(
+            self.envReport_uploading = self.load_value(
                 "Operations", "EnvReportUploading", DataType.BOOLEAN)
-            self.dayStat_uploading = load_value(
+            self.dayStat_uploading = self.load_value(
                 "Operations", "DayStatUploading", DataType.BOOLEAN)
-            self.camera_uploading = load_value(
+            self.camera_uploading = self.load_value(
                 "Operations", "CameraUploading", DataType.BOOLEAN)
-            self.local_server = load_value(
+            self.local_server = self.load_value(
                 "Operations", "LocalServer", DataType.BOOLEAN)
 
             # Load Sensors group values
-            self.log_AirT = load_value("Sensors", "LogAirT", DataType.BOOLEAN)
-            self.AirT_address = load_value(
-                "Sensors", "AirTAddress", DataType.STRING)
-            self.log_ExpT = load_value("Sensors", "LogExpT", DataType.BOOLEAN)
-            self.ExpT_address = load_value(
-                "Sensors", "EncTAddress", DataType.STRING)
-            self.log_RelH = load_value("Sensors", "LogRelH", DataType.BOOLEAN)
-            self.log_DewP = load_value("Sensors", "LogDewP", DataType.BOOLEAN)
-            self.log_WSpd = load_value("Sensors", "LogWSpd", DataType.BOOLEAN)
-            self.log_WDir = load_value("Sensors", "LogWDir", DataType.BOOLEAN)
-            self.log_WGst = load_value("Sensors", "LogWGst", DataType.BOOLEAN)
-            self.log_SunD = load_value("Sensors", "LogSunD", DataType.BOOLEAN)
-            self.log_Rain = load_value("Sensors", "LogRain", DataType.BOOLEAN)
-            self.log_StaP = load_value("Sensors", "LogStaP", DataType.BOOLEAN)
-            self.log_MSLP = load_value("Sensors", "LogMSLP", DataType.BOOLEAN)
-            self.log_ST10 = load_value("Sensors", "LogST10", DataType.BOOLEAN)
-            self.ST10_address = load_value(
-                "Sensors", "ST10Address", DataType.STRING)
-            self.log_ST30 = load_value("Sensors", "LogST30", DataType.BOOLEAN)
-            self.ST30_address = load_value(
-                "Sensors", "ST30Address", DataType.STRING)
-            self.log_ST00 = load_value("Sensors", "LogST00", DataType.BOOLEAN)
-            self.ST00_address = load_value(
-                "Sensors", "ST00Address", DataType.STRING)
-            self.log_EncT = load_value("Sensors", "LogEncT", DataType.BOOLEAN)
-            self.EncT_address = load_value(
-                "Sensors", "EncTAddress", DataType.STRING)
-            self.log_CPUT = load_value("Sensors", "LogCPUT", DataType.BOOLEAN)
+            (self.log_AirT
+                = self.load_value("Sensors", "LogAirT", DataType.BOOLEAN))
+            (self.AirT_address
+                = self.load_value("Sensors", "AirTAddress", DataType.STRING))
+            (self.log_ExpT
+                = self.load_value("Sensors", "LogExpT", DataType.BOOLEAN))
+            (self.ExpT_address
+                = self.load_value("Sensors", "EncTAddress", DataType.STRING))
+            (self.log_RelH
+                = self.load_value("Sensors", "LogRelH", DataType.BOOLEAN))
+            (self.log_DewP
+                = self.load_value("Sensors", "LogDewP", DataType.BOOLEAN))
+            (self.log_WSpd
+                = self.load_value("Sensors", "LogWSpd", DataType.BOOLEAN))
+            (self.log_WDir
+                = self.load_value("Sensors", "LogWDir", DataType.BOOLEAN))
+            (self.log_WGst
+                = self.load_value("Sensors", "LogWGst", DataType.BOOLEAN))
+            (self.log_SunD
+                = self.load_value("Sensors", "LogSunD", DataType.BOOLEAN))
+            (self.log_Rain
+                = self.load_value("Sensors", "LogRain", DataType.BOOLEAN))
+            (self.log_StaP
+                = self.load_value("Sensors", "LogStaP", DataType.BOOLEAN))
+            (self.log_MSLP
+                = self.load_value("Sensors", "LogMSLP", DataType.BOOLEAN))
+            (self.log_ST10
+                = self.load_value("Sensors", "LogST10", DataType.BOOLEAN))
+            (self.ST10_address
+                = self.load_value("Sensors", "ST10Address", DataType.STRING))
+            (self.log_ST30
+                = self.load_value("Sensors", "LogST30", DataType.BOOLEAN))
+            (self.ST30_address
+                = self.load_value("Sensors", "ST30Address", DataType.STRING))
+            (self.log_ST00
+                = self.load_value("Sensors", "LogST00", DataType.BOOLEAN))
+            (self.ST00_address
+                = self.load_value("Sensors", "ST00Address", DataType.STRING))
+            (self.log_EncT
+                = self.load_value("Sensors", "LogEncT", DataType.BOOLEAN))
+            (self.EncT_address
+                = self.load_value("Sensors", "EncTAddress", DataType.STRING))
+            (self.log_CPUT
+                = self.load_value("Sensors", "LogCPUT", DataType.BOOLEAN))
         except: return False
 
         return False if self.__validate() == False else True
