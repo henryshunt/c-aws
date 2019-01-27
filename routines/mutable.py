ed7533f288120ddde9354726a9bac0acd1e2f2a7
Henry Hunt <henryhunt.dev@gmail.com>
2019-01-27 16:15:37 +0000

    No longer needed since the switch to sensor classes

--- routines/mutable.py
+++
@@ -1,9 +0,0 @@
-class MutableValue():    
-    def __init__(self):
-        self.__value = None
-
-    def getValue(self):
-        return self.__value
-
-    def setValue(self, value):
-        self.__value = value
