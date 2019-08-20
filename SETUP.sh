cfedb1b7ea1cdeef9505cd09ca6a8a220d002bf1
Henry Hunt <henryhunt.dev@gmail.com>
2019-08-20 18:56:27 +0000

    Updated required dependencies

--- SETUP.sh
+++ SETUP.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
-echo "Installing dependencies for C-AWS..."
+echo "Installing C-AWS software dependencies..."
 
 apt-get install python3-rpi.gpio
-apt-get install python3-picamera
+apt-get install python3-apscheduler
 apt-get install python3-tz
 apt-get install python3-gpiozero
-apt-get install python3-apscheduler
-apt-get install python-spidev
+apt-get install python3-picamera
 
 apt-get install python3-pip
-pip3 install adafruit-blinka
-pip3 install Adafruit_GPIO
-pip3 install astral
 pip3 install python-daemon
+pip3 install adafruit-blinka
 pip3 install adafruit-circuitpython-mcp9808
 pip3 install adafruit-circuitpython-htu21d
 pip3 install adafruit-circuitpython-bme280
 pip3 install adafruit-circuitpython-mcp3xxx
+pip3 install astral
 
-echo "DONE installing dependencies for C-AWS"
+echo "Finished installing C-AWS software dependencies"
